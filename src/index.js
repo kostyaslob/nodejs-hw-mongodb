56f4c342597c0b15d38ef84585a91348fc4c61b7
kostyaslob <kostyaslob@gmail.com>
2025-07-05 21:08:07 +0300

    done step-3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import { setupServer } from "./server.js";
+import { initMongoConnection } from "./db/initMongoConnection.js";
 
-setupServer();
+const bootstrap = async () => {
+    await initMongoConnection();
+    setupServer();
+};
+
+bootstrap();
+
