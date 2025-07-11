fd526d871f71135effc63f95e2b0c4d1cf3d24b9
kostyaslob <kostyaslob@gmail.com>
2025-07-11 22:50:08 +0300

    add error middlewares

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAllContactsController, getContactByIdController } from "../controllers/contacts.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
 
-router.get("/contacts", getAllContactsController);
+router.get("/contacts", ctrlWrapper(getAllContactsController));
 
-router.get("/contacts/:contactId", getContactByIdController);
+router.get("/contacts/:contactId", ctrlWrapper(getContactByIdController));
 
 export default router;
