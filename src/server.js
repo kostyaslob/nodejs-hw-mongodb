3f3009935b4d9ecbe2a15cfc8abb5a43b224298b
kostyaslob <kostyaslob@gmail.com>
2025-07-25 23:54:45 +0300

    done step-4

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pino from "pino-http";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import router from "./routers/index.js";
 import { getEnvVar } from "./utils/getEnvVar.js";
@@ -19,6 +20,8 @@ export const setupServer = () => {
         }),
     );
     app.use(cors());
+
+    app.use(cookieParser());
 
     app.use(
         pino({
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,14 +1,18 @@
 import bcrypt from "bcrypt";
 import createHttpError from "http-errors";
+import { randomBytes } from "crypto";
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from "../constants/index.js";
 
 import { UsersCollection } from "../db/models/user.js";
+import { SessionCollection } from "../db/models/session.js";
 
 export const registerUser = async (payload) => {
     const user = await UsersCollection.findOne({
         email: payload.email
     });
 
-    if (user) throw createHttpError(409, "Email in use");
+    if (user)
+        throw createHttpError(409, "Email in use");
 
     const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
@@ -17,3 +21,32 @@ export const registerUser = async (payload) => {
         password: encryptedPassword,
     });
 };
+
+export const loginUser = async (payload) => {
+    const user = await UsersCollection.findOne({
+        email: payload.email
+    });
+
+    if (!user) {
+        throw createHttpError(401, "User not found");
+    }
+    const isEqual = await bcrypt.compare(payload.password, user.password);
+
+    if (!isEqual) {
+        throw createHttpError(401, "Unauthorized");
+    }
+
+    await SessionCollection.deleteOne({ userId: user._id });
+
+    const accessToken = randomBytes(30).toString("base64");
+    const refreshToken = randomBytes(30).toString("base64");
+
+    return await SessionCollection.create({
+        userId: user._id,
+        accessToken,
+        refreshToken,
+        accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+        refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS),
+    });
+};
+
