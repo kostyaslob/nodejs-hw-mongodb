322d179c50713a50f2c4ed309b99bbaf4c095b61
kostyaslob <kostyaslob@gmail.com>
2025-07-26 12:35:26 +0300

    done step-8, middleware authenticate

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -11,9 +11,12 @@ import {
 import { validateBody } from "../middlewares/validateBody.js";
 import { isValidId } from "../middlewares/isValidId.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get("/", ctrlWrapper(getAllContactsController));
 
