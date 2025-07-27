1e0a36aad4e627096772e9d9423408a1b980b3ab
kostyaslob <kostyaslob@gmail.com>
2025-07-27 15:10:21 +0300

    update-3 for step-8

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -50,28 +50,29 @@ export const createContact = async (payload) => {
     return contact;
 };
 
-export const deleteContact = async (contactId) => {
-    const contact = await ContactsCollection.findOneAndDelete({
-        _id: contactId,
-    });
-    return contact;
+export const deleteContact = async (contactId, userId) => {
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+    userId,
+  });
+  return contact;
 };
 
-export const updateContact = async (contactId, payload, options = {}) => {
-    const rawResult = await ContactsCollection.findByIdAndUpdate(
-        { _id: contactId },
-        payload,
-        {
-            new: true,
-            includeResultMetadata: true,
-            ...options
-        }
-    );
+export const updateContact = async (contactId, userId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId, userId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
 
-    if (!rawResult || !rawResult.value) return null;
+  if (!rawResult || !rawResult.value) return null;
 
-    return {
-        contact: rawResult.value,
-        isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-    };
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
 };
