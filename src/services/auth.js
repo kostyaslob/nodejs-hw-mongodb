44f949c303d0559afb91a97cc089b10a6409ee7b
kostyaslob <kostyaslob@gmail.com>
2025-08-02 19:55:23 +0300

    done step-3

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,11 @@
 import bcrypt from "bcrypt";
 import createHttpError from "http-errors";
 import jwt from "jsonwebtoken";
+import handlebars from "handlebars";
+import path from "node:path";
+import fs from "node:fs/promises";
 import { randomBytes } from "crypto";
-import { FIFTEEN_MINUTES, THIRTY_DAYS, SMTP } from "../constants/index.js";
+import { FIFTEEN_MINUTES, THIRTY_DAYS, SMTP, TEMPLATES_DIR } from "../constants/index.js";
 import { getEnvVar } from "../utils/getEnvVar.js";
 import { sendEmail } from "../utils/sendMail.js";
 
@@ -113,10 +116,29 @@ export const sendResetToken = async (email) => {
     },
   );
 
+  const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    "reset-password-email.html",
+  );
+
+  const templateSource = (
+    await fs.readFile(resetPasswordTemplatePath)).toString();
+
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.name,
+    link: `${getEnvVar("APP_DOMAIN")}/reset-password?token=${resetToken}`,
+  });
+
+  try {
     await sendEmail({
         from: getEnvVar(SMTP.SMTP_FROM),
         to: email,
         subject: "Reset your password",
-        html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`
+        html,
     });
+  } catch {
+    throw createHttpError(500, "Failed to send the email, please try again later.",
+    );
+  }
 };
